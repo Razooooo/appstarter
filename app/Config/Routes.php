c4bee81991e93a0cc584d18e66315c86134b82f1
Razafindrakoto Nathan <Nathan.Raza18@gmail.com>
2024-04-30 11:21:30 +0200

    routes à jour

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,23 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->get("/","Home::index", ["filter" => \App\Filters\AuthenticatedFilter::class]);
+$routes->get("/home","Home::index", ["filter" => \App\FiltersAuthenticatedFilter::class]);
+$routes->get("/abonnes", "Abi=onne::index", ["filter" => 'isAdmin']);
+$routes->post("/abonnes/(:num)", "Abonne::updateAbonne", ["filter" => "isAdmin"]);
+$routes->get("/abonnes/(:num)", "Abonne::modifyAbonneForm", ["filter" => "isAdmin"]);
+$routes->get("/abonnes/(:num)/delete", "Abonne::deleteAbonneform", ["filter" => "isAdmin"]);
+$routes->post("/abonnes/(:num)/delete", "Abonne::deleteAbonne", ["filter" => "isAdmin"]);
+$routes->get("/abonnes/new", "Abonne::createAbonneForm", ["filter" => "isAdmin"]);
+$routes->post("/abonnes", "Abonne::addAbonne", ["filter" => "isAdmin"]);
+$routes->get("/livres", "Livre::index", ["filter" => "authentifcated"]);
+$routes->get("/livres/new", "Livres::createLivreForm", ["filter" => "isAdmin"]);
+$routes->post("/livres", "Livre::addLivre", ["filter" => "isAdmin"]);
+$routes->get("/livres/(:num)", "Livre::modifyLivreForm", ["filter" => "isAdmin"]);
+$routes->get("/login", "Connection::loggout");
 $routes->get('/login','Connection::index');
 $routes->post('/login', 'Connection::attemptLogin');
-$routes->get('/home','Home::index', ['filter' => \App\FiltersAuthenticatedFilter::class]);
-$routes->get('/','Home::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
+$routes->get("/login", "Connection::loggout");
+
+
+$routes->get("/api/auteurs", "Auteur::getAuteurs");
